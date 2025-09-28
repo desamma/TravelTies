29f2604442f0cd4509d4bbc41f518dccf971e118
JT2k4 <142780410+JT2k4@users.noreply.github.com>
2025-09-29 06:24:57 +0700

    Support
    Đã làm Support cho customer

---
+++ TravelTies/Areas/Customer/Controllers/SupportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelTies.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+
+public class SupportController : Controller
+{
+    [HttpGet("/support")]
+    public IActionResult Index() => View();
+}
+}
